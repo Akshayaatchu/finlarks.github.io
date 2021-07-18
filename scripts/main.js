436701d6ca6e22b427ef9b24c0ce072e27df5c12
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2021-07-18 18:39:19 +0530

    New Look in Nav

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,31 @@
  * @brief Scroll to Event
  */
 function scrollEvent(evt) {
-    var scrollTop = $(window).scrollTop();
-    var scrollDel = $(window).height();
-
-    if (scrollTop > scrollDel * 0.5) {
-        $("header nav").addClass("nav-theme");
+    if ($(window).scrollTop() > 0) {
+        $("header").removeClass("theme-static").addClass("theme-scroll");
     } else {
-        $("header nav").removeClass("nav-theme");
+        $("header").removeClass("theme-scroll").addClass("theme-static");
+    }
+}
+
+/**
+ * @brief Toggles Menu
+ */
+function menuToggle(evt) {
+    menubtn = $("#menu-btn");
+    menubar = $("#menu-bar");
+    menucon = $("#menu-con");
+
+    if(menubar.width() == 0) {
+        menubar.width(300);
+        menucon.fadeIn();
+        menubar.css('box-shadow', '0 0 0 100vw rgba(0, 0, 0, 0.5');
+        menubtn.html('<i class="fas fa-arrow-right"></i>');
+    } else {
+        menucon.fadeOut();
+        menubar.width(0);
+        menubar.css('box-shadow', '0 0 0 0 rgba(0, 0, 0, 0.5');
+        menubtn.html('<i class="fas fa-bars"></i>');
     }
 }
 
@@ -17,4 +35,5 @@ function scrollEvent(evt) {
  */
 function main() {
     $(window).on("scroll", scrollEvent);
+    $("#menu-btn").on('click', menuToggle);
 }
