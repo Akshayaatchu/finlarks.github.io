830ca274f303806b84cd50e26f8ee907eff6f787
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2021-08-10 23:54:19 +0530

    Fix

--- public/index.php
+++ public/index.php
@@ -29,7 +29,6 @@
     
     <meta name="content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
 
     <link rel="stylesheet" href="styles/main.css" />
     <script src="scripts/main.js"> </script>
